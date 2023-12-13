5acfe5574f5d0328d58bda47f817dc6c0be67b76
Sai swaroop <104573154+swaroop9010@users.noreply.github.com>
2023-12-13 18:42:50 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect('organizations.db')
+    cursor = conn.cursor()
+
+    # Create industries table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS industries (
+            industry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            industry_name TEXT NOT NULL,
+            age INTEGER,
+            division_id INTEGER,
+            FOREIGN KEY (division_id) REFERENCES divisions(division_id)
+        )
+    ''')
+
+    # Create divisions table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS divisions (
+            division_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            division_name TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
